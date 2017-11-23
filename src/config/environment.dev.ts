cb4ac3c808c05ef8ab57001d4f6a1060f8e77e30
Gerard Soldevila <soldevila@gmail.com>
2017-11-23 10:46:27 +0100

    Added missing config files

---
+++ src/config/environment.dev.ts
@@ -0,0 +1,2 @@
+export const PRODUCTION: boolean = false;
+export const SOCKET_IO_URL: string = ':16969';
---
+++ src/config/environment.prod.ts
@@ -0,0 +1,2 @@
+export const PRODUCTION: boolean = true;
+export const SOCKET_IO_URL: string = '/';
